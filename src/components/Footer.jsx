a56a471606910be1d40f7884e351eadf3719f903
vijay0467 <vijayreddypatel802@gmail.com>
2025-01-22 11:29:54 +0530

    changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <footer className="w-full  bg-gray-800 bg-opacity-50 font-Parkinsans text-white py-4 text-center">
-        <p>&copy; 2025 AnchorAudit. All rights reserved.</p>
+        <p>&copy; 2025 FindMyCA. All rights reserved.</p>
       </footer>
     </div>
   )

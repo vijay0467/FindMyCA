2213db0f40242ce8927beb6b33b64aee1cf93fb1
vijay0467 <vijayreddypatel802@gmail.com>
2025-02-19 15:54:07 +0530

    first commit changed

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000'; // URL for JSON Server
+const API_BASE_URL = 'https://find-my-ca-coral.vercel.app/'; // URL for JSON Server
 
 export const fetchAccountants = async (query) => {
   try {
